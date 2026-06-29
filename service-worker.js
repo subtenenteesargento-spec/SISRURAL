fadecd09d72a2a13a44bd16f6125eb81e57f5300
subtenenteesargento-spec <subtenenteesargento@gmail.com>
2026-06-29 07:20:10 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sisrural-v7-1-admin-fix';
+const CACHE = 'sisrural-v7-3-motor-firestore';
 const ASSETS = ['./','./index.html','./offline.html','./manifest.webmanifest','./config.firebase.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim())));
